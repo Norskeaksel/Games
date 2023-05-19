e93d5f12d65928693293bbead96e743668918326
Aksel <ahsorbye@gmail.com>
2023-05-19 19:24:49 +0200

    Upload Minesweeper files

--- Minesweeper/Minesweeper/main.cpp
+++ Minesweeper/Minesweeper/main.cpp
@@ -6,9 +6,7 @@ int main(){
 	srand(time(NULL));
 	int width=10, height=10, mines=10;
 	cout << "Welcome to Minesweeper!\n";
-	//Sleep(2000);
 	cout << "In order to win you most open the last tile, not flagg it. Remember that and lets get started!\n";
-	//Sleep(3500);
 	cout<<"Choose a width (a number between 6 and 42 is recommended): ";
 	cin >> width;
 	cout << "\nChoose a height (a number between 1 and 19 is recommended): ";
@@ -19,5 +17,4 @@ int main(){
 	Point startPoint{ 0,0 };
 	MinesweeperWindow mw{ startPoint, width, height, mines, "Minesweeper" };
 	return gui_main();
-
 }
